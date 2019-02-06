bc2e3e222f6aeecd35e2f352f5af8c535de9ba84
Astrid Corena <astrid.corena@gmail.com>
2019-02-06 17:14:06 -0500

    first commit

--- solution.rb
+++ solution.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 
 get '/' do
   unless params[:nombre]
-  "Hola desconocido!"
+  erb "<h1>Hola desconocido!</h1>"
 else
   erb "<h1>Hola<%= nombre %></h1>", { :locals => params }
 end
