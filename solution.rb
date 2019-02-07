724d458e7c3abc9189945e204f92b7b59093831b
Astrid Corena <astrid.corena@gmail.com>
2019-02-06 23:18:20 -0500

    first commit

--- solution.rb
+++ solution.rb
@@ -2,9 +2,10 @@ require 'sinatra'
 require 'erb'
 
 get '/' do
-  unless params[:nombre]
+  if params[:nombre].nil? || params[:nombre]== ""
   erb "<h1>Hola desconocido!</h1>"
 else
   erb "<h1>Hola #{params[:nombre]}!</h1>"
+
 end
 end
